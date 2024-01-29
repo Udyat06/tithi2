87688df52778a7db9b4ca06b81379e07e8cb17fe
Udyat06 <149685789+Udyat06@users.noreply.github.com>
2024-01-30 00:10:16 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function f1(){
     const username = document.getElementById("u").value
     const password = document.getElementById("p").value 
 
-    if (username == "tithiiwho" & password == "udyattithi06"){
+    if (username == "tithiiwho" & password == "udyattithi3"){
         alert("Good! I miss you please text back")}
     else{
         alert("WTF! Who are you Hint: Use your insta username and password")
